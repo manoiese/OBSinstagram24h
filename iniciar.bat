a8226aacc53150c32706d3c5a99fb02e17ad9b77
BENEDITO GUSTAVO LOBO <31225572+manoiese@users.noreply.github.com>
2022-04-14 19:52:04 -0300

    Update iniciar.bat

--- iniciar.bat
+++ iniciar.bat
@@ -1,5 +1,5 @@
 @echo off
 :reinicio
-python live_broadcast_ffmpeg.py -u riovermelhofm -p rvermelho967 -f C:\WEBSERVER\root\stream\live.m3u8
+python live_broadcast_ffmpeg.py -u usuarioinstagram -p senhainstagram -f C:\WEBSERVER\root\stream\live.m3u8
 goto reinicio
 exit
